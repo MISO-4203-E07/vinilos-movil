3f8af0c132f2eb74b3df91f1a2973853570dd98e
Yesid Tombe <y.tombe@uniandes.edu.co>
2021-10-26 00:50:55 -0500

    Base app implemented

---
+++ app/src/main/java/co/edu/uniandes/vinilos/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.edu.uniandes.vinilos.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.edu.uniandes.vinilos.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
