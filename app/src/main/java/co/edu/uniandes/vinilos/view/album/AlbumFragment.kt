fda33a0a9570197e5e695b34c84c586e253f309d
Andrés Aldana <a.aldanam@uniandes.edu.co>
2021-11-17 20:45:49 -0500

    Feat: adding feature artist detail (#38)

--- app/src/main/java/co/edu/uniandes/vinilos/view/album/AlbumFragment.kt
+++ app/src/main/java/co/edu/uniandes/vinilos/view/album/AlbumFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import co.edu.uniandes.vinilos.databinding.FragmentAlbumBinding
 import co.edu.uniandes.vinilos.viewmodel.AlbumViewModel
 
--- app/src/main/java/co/edu/uniandes/vinilos/view/artist/ArtistFragment.kt
+++ app/src/main/java/co/edu/uniandes/vinilos/view/artist/ArtistFragment.kt
@@ -1,12 +1,12 @@
 package co.edu.uniandes.vinilos.view.artist
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import co.edu.uniandes.vinilos.databinding.FragmentArtistBinding
 import co.edu.uniandes.vinilos.viewmodel.ArtistViewModel
 
@@ -40,7 +40,10 @@ class ArtistFragment : Fragment() {
     }
 
     private fun goToDetailArtist(id: Int) {
-      //TODO DETAIL HU004
+        val intent = Intent(binding.root.context, ArtistDetailActivity::class.java).apply {
+            putExtra("ID_ARTIST", id)
+        }
+        startActivity(intent)
     }
 
     override fun onDestroyView() {
