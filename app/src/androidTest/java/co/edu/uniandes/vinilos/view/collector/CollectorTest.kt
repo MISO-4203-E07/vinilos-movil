b213b5f8878edce29c6d94c1b591eabfc296d0cf
J Andres Boyaca Silva <j.boyaca@uniandes.edu.co>
2021-12-05 12:02:54 -0500

    Add Collector testing

--- app/src/androidTest/java/co/edu/uniandes/vinilos/view/collector/CollectorTest.kt
+++ app/src/androidTest/java/co/edu/uniandes/vinilos/view/collector/CollectorTest.kt
@@ -4,6 +4,7 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
@@ -78,6 +79,114 @@ class CollectorTest : BaseTest() {
             .check(matches(atPosition(100, not(isDisplayed()))))
     }
 
+
+    @Test
+    fun givenCollector_WhenCollectorIsValid_SeeDetails() {
+        Thread.sleep(TimeUnit.SECONDS.toMillis(2))
+
+        getNavBar().perform(click())
+
+        Thread.sleep(TimeUnit.SECONDS.toMillis(1))
+
+        getItemNavBar(R.id.nav_collector).perform(click())
+
+        onView(withId(R.id.rvCollectors)).check(matches(atPosition(100, not(isDisplayed()))))
+
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.rvCollectors),
+                childAtPosition(
+                    withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                    0
+                )
+            )
+        )
+        recyclerView.perform(RecyclerViewActions.actionOnItemAtPosition<ViewHolder>(0, click()))
+
+        val viewGroup = onView(
+            allOf(
+                withParent(
+                    allOf(
+                        withId(android.R.id.content),
+                        withParent(withId(R.id.decor_content_parent))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        viewGroup.check(matches(isDisplayed()))
+    }
+
+
+    @Test
+    fun givenCollector_WhenCollectorIsValid_SeeDetailsOfAlbumAndReturn() {
+        Thread.sleep(TimeUnit.SECONDS.toMillis(2))
+
+        getNavBar().perform(click())
+
+        Thread.sleep(TimeUnit.SECONDS.toMillis(1))
+
+        getItemNavBar(R.id.nav_collector).perform(click())
+
+        onView(withId(R.id.rvCollectors)).check(matches(atPosition(100, not(isDisplayed()))))
+
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.rvCollectors),
+                childAtPosition(
+                    withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                    0
+                )
+            )
+        )
+        recyclerView.perform(RecyclerViewActions.actionOnItemAtPosition<ViewHolder>(0, click()))
+
+        val viewGroup = onView(
+            allOf(
+                withParent(
+                    allOf(
+                        withId(android.R.id.content),
+                        withParent(withId(R.id.decor_content_parent))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        viewGroup.check(matches(isDisplayed()))
+        val recyclerView2 = onView(
+            allOf(
+                withId(R.id.rvCollectorAlbums),
+                childAtPosition(
+                    withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                    4
+                )
+            )
+        )
+        recyclerView2.perform(RecyclerViewActions.actionOnItemAtPosition<ViewHolder>(0, click()))
+
+        returnAction();
+        returnAction();
+
+        val recyclerView3 = onView(
+            allOf(
+                withId(R.id.rvCollectors),
+                withParent(withParent(withId(R.id.nav_host_fragment_content_main))),
+                isDisplayed()
+            )
+        )
+        recyclerView3.check(matches(isDisplayed()))
+
+        val recyclerView4 = onView(
+            allOf(
+                withId(R.id.rvCollectors),
+                childAtPosition(
+                    withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                    0
+                )
+            )
+        )
+        recyclerView4.perform(RecyclerViewActions.actionOnItemAtPosition<ViewHolder>(0, click()))
+    }
 
 
 }
